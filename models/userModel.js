852634b76779466f18934c7b112354b05a808c0e
SagiSan <samir.sagi@gmail.com>
2019-01-27 22:33:04 +0100

    is questionnaire done + leftovers on submission (add submission to user)

--- models/userModel.js
+++ models/userModel.js
@@ -1,11 +1,13 @@
 var mongoose = require("mongoose");
+var arrayUniquePlugin = require('mongoose-unique-array');
 var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
   isAdmin: { type: Boolean, required: true },
   username: { type: String, required: true, index: { unique: true } },
   password: { type: String, required: true },
-  doneQuestionnaires: [{ type: Schema.Types.ObjectId, ref: "submission" }]
+  doneQuestionnaires: [{ type: Schema.Types.ObjectId, ref: "submission", unique: true }]
 });
+userSchema.plugin(arrayUniquePlugin);
 
 module.exports = mongoose.model("user", userSchema);
